d0dfce07fbf1a0c6a9e0140dc367de307b821103
Dung <dung0158234@gmail.com>
2024-01-31 23:26:45 +0700

    handle screen rotation

--- play-around/src/main/java/com/example/play_around/MainActivity.java
+++ play-around/src/main/java/com/example/play_around/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.play_around;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView text_View;
+    private FloatingActionButton btn_add;
+    private MyViewModel model;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        model = new ViewModelProvider(this).get(MyViewModel.class);
+        text_View = findViewById(R.id.text_View);
+        btn_add = findViewById(R.id.btn_add);
+
+        model.getNumber().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                text_View.setText("" + integer);
+            }
+        });
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                model.increaseNumber();
+            }
+        });
     }
 }
---
+++ play-around/src/main/java/com/example/play_around/MyViewModel.java
@@ -0,0 +1,19 @@
+package com.example.play_around;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MyViewModel extends ViewModel {
+    private MutableLiveData<Integer> number;
+    public LiveData<Integer> getNumber(){
+        if(number == null){
+            number = new MutableLiveData<>();
+            number.setValue(0);
+        }
+        return number;
+    }
+    public void  increaseNumber(){
+        number.setValue(number.getValue()+1);
+    }
+}
