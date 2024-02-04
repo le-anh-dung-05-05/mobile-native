cf95fc76d782eba905dc3098eab20e69c3185b8b
Dung <dung0158234@gmail.com>
2024-02-04 23:54:33 +0700

    detail activity

--- play-around/src/main/java/com/example/play_around/MainActivity.java
+++ play-around/src/main/java/com/example/play_around/MainActivity.java
@@ -4,16 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private TextView text_View;
     private FloatingActionButton btn_add;
     private MyViewModel model;
+    private ListView list_View;
+    private ArrayList<String> arrayList;
+    private ArrayAdapter<String> arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +30,16 @@ public class MainActivity extends AppCompatActivity {
         model = new ViewModelProvider(this).get(MyViewModel.class);
         text_View = findViewById(R.id.text_View);
         btn_add = findViewById(R.id.btn_add);
-
+        list_View = findViewById(R.id.list_view);
+        arrayList = new ArrayList<String>();
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arrayList);
+        list_View.setAdapter(arrayAdapter);
         model.getNumber().observe(this, new Observer<Integer>() {
             @Override
             public void onChanged(Integer integer) {
                 text_View.setText("" + integer);
+                arrayList.add(""+integer);
+                arrayAdapter.notifyDataSetChanged();
             }
         });
         btn_add.setOnClickListener(new View.OnClickListener() {
@@ -34,5 +48,22 @@ public class MainActivity extends AppCompatActivity {
                 model.increaseNumber();
             }
         });
+        list_View.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener(){
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                arrayList.remove(position);
+                arrayAdapter.notifyDataSetChanged();
+                return  false;
+            }
+        });
+        list_View.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.putExtra("number", arrayList.get(position).toString());
+                startActivity(intent);
+            }
+        });
+
     }
 }
